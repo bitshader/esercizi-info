f6e9d171a52d1f14ec81c62bec94e53b66cf973d
Alessandro Buggin <alessandrobuggin@gmail.com>
2018-10-31 16:44:55 +0100

    format minore.cpp

--- minore.cpp
+++ minore.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 using namespace std;
- 
-int main() {
-int min=0, n;
 
-cout << "Inserisci la dimensione: ";
-cin >> n;
-int v[n];
-
-for(int i=0; i<n; i++) 
+int main()
 {
-	cout<< "Inserisci un valore: ";
-	cin>> v[i];
-}
+	int min = 0, n;
 
-min = v[0];
+	cout << "Inserisci la dimensione: ";
+	cin >> n;
+	int v[n];
 
-for(int i=0; i<n; i++)
-{
-	if(min>v[i])
- 	min=v[i];
-}
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Inserisci un valore: ";
+		cin >> v[i];
+	}
 
-cout << "L'elemento minimo del vettore e'	" << min;
-cout << "\n";
-return 0;
+	min = v[0];
+
+	for (int i = 0; i < n; i++)
+	{
+		if (min > v[i])
+			min = v[i];
+	}
+
+	cout << "L'elemento minimo del vettore e'	" << min;
+	cout << "\n";
+	return 0;
 }
