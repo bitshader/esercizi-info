442709a659d5bfb97fed221da95ad5b854f35a3d
Alessandro Buggin <alessandrobuggin@gmail.com>
2018-10-31 17:15:02 +0100

    fail not to fail

--- minore.test.sh
+++ minore.test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-rm eseguibile
-rm out
+rm -f eseguibile
+rm -f out
+
 g++ -o eseguibile minore.cpp
+
 ./eseguibile <<< '4 2 3 4 5' > out
 RETURN_VALUE="$(tail -c 1 out)"
 EXPECTED_VALUE=8
+
 if [ $RETURN_VALUE -eq $EXPECTED_VALUE ]
 then 
     echo "ok"
