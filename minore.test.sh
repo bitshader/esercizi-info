3269e654fd377ce8b6ccc99757c64cd41cee8750
Alessandro Buggin <alessandrobuggin@gmail.com>
2018-10-31 17:20:10 +0100

    due test per minore

--- minore.test.sh
+++ minore.test.sh
@@ -8,13 +8,28 @@ g++ -o eseguibile minore.cpp
 
 ./eseguibile <<< '4 2 3 4 5' > out
 RETURN_VALUE="$(tail -c 1 out)"
-EXPECTED_VALUE=8
+EXPECTED_VALUE=2
 
 if [ $RETURN_VALUE -eq $EXPECTED_VALUE ]
 then 
     echo "ok"
-    exit 0;
 else 
     echo "fail"
+    echo -e "\tgot value: $RETURN_VALUE"
+    echo -e "\texpected value: $EXPECTED_VALUE"
+fi
+
+
+./eseguibile <<< '5 5 5 5 5 5' > out
+RETURN_VALUE="$(tail -c 1 out)"
+EXPECTED_VALUE=5
+
+if [ $RETURN_VALUE -eq $EXPECTED_VALUE ]
+then 
+    echo "ok"
+else 
+    echo "fail"
+    echo -e "\tgot value: $RETURN_VALUE"
+    echo -e "\texpected value: $EXPECTED_VALUE"
     exit -1;
 fi
